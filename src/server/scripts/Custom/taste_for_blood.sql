73d6b11bd9d2ad4d6fdbd0f3926553212d49e1fc
xDevICCI <vgodoy.work@gmail.com>
2018-11-16 11:43:42 -0600

    taste_for_blood_warrior
    proc no se encontraban para:
    
    Taste for Blood ( rank 1)
    Taste for Blood ( rank 2)
    Taste for Blood ( rank 3)

---
+++ src/server/scripts/Custom/taste_for_blood.sql
@@ -0,0 +1,4 @@
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`)
+VALUES ( 56636, 0x00,   4, 0x00000020, 0x00000000, 0x00000000, 0x00000000, 0x00040000,   0,   0,   6), -- Taste for Blood (Rank 1)
+( 56637, 0x00,   4, 0x00000020, 0x00000000, 0x00000000, 0x00000000, 0x00040000,   0,   0,   6), -- Taste for Blood (Rank 2)
+( 56638, 0x00,   4, 0x00000020, 0x00000000, 0x00000000, 0x00000000, 0x00040000,   0,   0,   6); -- Taste for Blood (Rank 3)
