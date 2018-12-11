e85c3b330603dd069b11c36c3042ff5656c36bf5
xDevICCI <vgodoy.work@gmail.com>
2018-12-10 20:03:35 -0600

    [DK] FIX Scourge Strike
    http://wotlk.cavernoftime.com/spell=69961
    
    Your Scourge Strike increases the duration of your diseases on the target by 3 sec, up to a maximum of 9 additional seconds.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6058,6 +6058,13 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                     victim->RemoveAurasWithMechanic(1<<MECHANIC_STUN, AURA_REMOVE_BY_ENEMY_SPELL);
                     return true;
                 }
+                // Glyph of Scourge Strike
+                case 58642:
+                {
+                    // wotlk.cavernoftime.com/spell=69961
+                    triggered_spell_id = 69961; // Glyph of Scourge Strike
+                    break;
+                }
                 // Glyph of Life Tap
                 case 63320:
                 {
