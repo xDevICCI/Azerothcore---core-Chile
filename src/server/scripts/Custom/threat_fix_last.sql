2be996f4d624b27f802ff5a21d3b5e2f11e5484a
xDevICCI <vgodoy.work@gmail.com>
2018-12-13 20:38:37 -0600

    Revert "Threat para clases"
    This reverts commit 771591bbc6ad65148a16935885fcb27b8b817cdf.

--- src/server/scripts/Custom/threat_fix_last.sql
+++
@@ -1,9 +0,0 @@
--- Spell threat entries fix
-DELETE FROM `spell_threat` WHERE `entry` IN (52372,61411,53595,20187,48818,47520,47488,20243,12809,48568);
-INSERT INTO `spell_threat` (`entry`, `flatMod`, `pctMod`, `apPctMod`) VALUES 
-(52372, 0, 7.00, 0.0), -- Icy Touch
-(47520, 225, 1.00, 0.0), -- Cleave (Rank 8)
-(47488, 770, 1.30, 0.0), -- Shield Slam (Rank 8)
-(20243, 315, 1.00, 0.05), -- Devastate (Rank 1)
-(12809, 0, 2.00, 0.0), -- Concussion Blow
-(48568, 1031, 0.50, 0.0); -- Lacerate (Rank 3)
