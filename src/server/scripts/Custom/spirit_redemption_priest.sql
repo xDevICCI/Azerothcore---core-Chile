d3b44198b5e462269d5396479a1844a1a90f4019
xDevICCI <vgodoy.work@gmail.com>
2018-12-13 20:37:03 -0600

    Revert "Add Spell Spirit of Redemption Faltante en Priest"
    This reverts commit 9e3604dfeae5dbe97e462c7c7624a8d55107b9bf.

--- src/server/scripts/Custom/spirit_redemption_priest.sql
+++
@@ -1,4 +0,0 @@
-SET @ENTRY := 27827;
-DELETE FROM spell_script_names WHERE spell_id = @ENTRY;
-INSERT INTO spell_script_names (spell_id, ScriptName) VALUES
-(@ENTRY, 'spell_priest_spirit_of_redemption');
